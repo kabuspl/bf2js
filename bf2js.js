ad63e91ca34c39a7bc3b0aa5652942cb55fb41c1
kabuspl <kabuspl@outlook.com>
2022-02-13 14:14:02 +0100

    add code optimization

--- bf2js.js
+++ bf2js.js
@@ -1,34 +1,49 @@
 const bf2js = {
     convert: function(code) {
         let jsCode = "function bf(i=''){let j=0,p=0,m=[],o='';m[0]=0;"
+        let optimized = [];
         for (let i = 0; i < code.length; i++) {
-            switch (code[i]) {
-                case "<":
-                    jsCode += "p--;if(m[p]==undefined)m[p]=0;";
-                    break;
-                case ">":
-                    jsCode += "p++;if(m[p]==undefined)m[p]=0;";
-                    break;
-                case "+":
-                    jsCode += "m[p]++;";
-                    break;
-                case "-":
-                    jsCode += "m[p]--;";
-                    break;
-                case ".":
-                    jsCode += "o+=String.fromCharCode(m[p]);";
-                    break;
-                case ",":
-                    jsCode += "m[p]=i.charCodeAt(j);j++;";
-                    break;
-                case "[":
-                    jsCode += "while(m[p]){";
-                    break;
-                case "]":
-                    jsCode += "}";
-                    break;
-                default:
-                    break;
+            if (/^[\<\>\+\-\.\,\[\]]/.test(code[i])) {
+                if (i > 0 && optimized.at(-1).function == code[i])
+                    optimized.at(-1).count++;
+                else
+                    optimized.push({ function: code[i], count: 1 });
+            }
+        }
+        console.log(optimized);
+        for (let fun of optimized) {
+            if (/^[\<\>\+\-]/.test(fun.function)) {
+                switch (fun.function) {
+                    case "<":
+                        jsCode += "p-=" + fun.count + ";if(m[p]==undefined)m[p]=0;";
+                        break;
+                    case ">":
+                        jsCode += "p+=" + fun.count + ";if(m[p]==undefined)m[p]=0;";
+                        break;
+                    case "+":
+                        jsCode += "m[p]+=" + fun.count + ";";
+                        break;
+                    case "-":
+                        jsCode += "m[p]-=" + fun.count + ";";
+                        break;
+                }
+            } else {
+                for (let i = 0; i < fun.count; i++) {
+                    switch (fun.function) {
+                        case ".":
+                            jsCode += "o+=String.fromCharCode(m[p]);";
+                            break;
+                        case ",":
+                            jsCode += "m[p]=i.charCodeAt(j);j++;";
+                            break;
+                        case "[":
+                            jsCode += "while(m[p]){";
+                            break;
+                        case "]":
+                            jsCode += "}";
+                            break;
+                    }
+                }
             }
         }
         jsCode += "return o;}";
